e4230ed6e5a69c7760dce1e394ad59862c87978f
foglii <100421246+foglii@users.noreply.github.com>
2023-11-27 21:06:50 +0100

    added demod RX.m
    aggiunta demodulazione del segnale

--- RX.m
+++ RX.m
@@ -10,23 +10,12 @@ rxPluto = sdrrx('Pluto','RadioID',...
    
 tic;   
 [rxWave,rx_meta]=capture(rxPluto,1e6);
-toc;
-
+% toc;
 % The samples collected are now available in rxWave
-%% Plots
-% this will plot the absolute value of the samples
-figure (1)
-plot(abs(rxWave));
-grid on
-%rolloff_factor = 0.25; % Fattore di roll-off del filtro
-% 
-% % Design del filtro a coseno rialzato span = 9; % Lunghezza in simboli
-% del filtro sps = 8;  % Campionamenti per simbolo (oversampling factor)
-% rcosine_filter = rcosdesign(rolloff_factor, span,sps,'sqrt'); %
-% figure(2); % plot(rcosine_filter,'.'); rxWave=double(rxWave);
-% 
-% rcosine_filter = rcosdesign(rolloff_factor, span, sps,'sqrt'); rx_signal
-% = upfirdn(rxWave, rcosine_filter,1,sps);
+beta= 0.5; % Fattore di roll-off del filtro
+span = 6; % Lunghezza in simboli del filtro
+sps = 8;  % Campionamenti per simbolo (oversampling factor)
+
 rxfilter = comm.RaisedCosineReceiveFilter( ...
   'Shape','Square root', ...
   'RolloffFactor',beta, ...
@@ -34,46 +23,41 @@ rxfilter = comm.RaisedCosineReceiveFilter( ...
   'InputSamplesPerSymbol',sps, ...
   'DecimationFactor',sps);
 
-rx_signal=rxfilter(rxWave); %correggere con rxWave 
+rx_signal=rxfilter(rxWave);
 
 %correzione delay tx
 %rx_signal= rx_signal((span*sps)+1:end);
-
-
 t4=[0:1:length(rxWave)-1];
-figure(3);
+figure;
 plot(rxWave);
-title('segnale ricevuto');
-figure(4);
+title('costellazione');
+figure;
 plot(t4,rxWave);
-title('segnale ricevuto nel tempo');
+title('rxWave');
 xlabel('t');
 figure;
 t5=[0:1:length(rx_signal)-1];
 plot(t5,rx_signal);
-title('segnale filtrato');
+title('rx_signal');
 xlabel('t')
-% %correzione ritardo di 9 campioni(dimensione filtro)
-% for i=1:length(rx_signal)-(2*span)
-%     rx_signal_no_offset(i)=rx_signal(i+span);
-% end
+rxNorm=abs(rx_signal)/max(abs(rx_signal));
+figure;
+t6=[0:1:length(rxNorm)-1];
+plot(t6,rxNorm);
+title('rxNorm');
+xlabel('t')
+sig_demod=zeros(2000,1);
+%trovo il primo valore della sequenza
+i=1;
+while rxNorm(i)<0.5
+    i=i+1;
+end
 %demodulazione
-sig_demod=pamdemod(rx_signal_no_offset,2);
-
-
-
-
-%Calculate quantization noise power 
-%(assuming constant input)
-%Pnoise=10*log10(mean((real(rxWave)...
- %   - mean(real(rxWave))).^2));
-%disp("Detected Pnoise:")
-% disp(Pnoise)
-
-% Theoretical Pnoise N=12bit ADC
-% delta^2 / 12
-% delta is D/2^N
-% D=2
-% Pnt=10*log10(((2/2^12)^2)*(1/12));
-% disp("Theoretical Pnoise:")
-% disp(Pnt)
+c=i-1;
+for i=1:1:2000; %sappiamo che il nostro segnale è lungo 2000 campioni dopo lo zero padding
+    if abs(rxNorm(i+c))>0.5
+       sig_demod(i)=1;
+    else sig_demod(i)=0;
+    end
+plot(sig_demod)
+end
