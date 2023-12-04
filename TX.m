17af5c94e7a84589bdb22afd5d134f405b68b61e
foglii <100421246+foglii@users.noreply.github.com>
2023-12-04 19:37:52 +0100

    Update TX.m

--- TX.m
+++ TX.m
@@ -1,93 +1,120 @@
-SamplingRate=1e6;
-fc=2.44e9;
-idTX='sn:1044735411960009090038004f371e8d27';
+%% Trasmettitore
+
+close all;
+clear;
+
+% Specifiche Adalm Pluto
+findPlutoRadio
+idTX='usb:0';
+
+%% Definizione Parametri
+SamplingRate=1e6; % Frequenza di campionamento (Hz)
+fc=2.475e9;
 Nsimboli=1000;
-symbols=zeros(Nsimboli,1);
-symbols(500)=1;
-symbols(497)=1;
-symbols(750)=1;
-%symbols = randi([0, 1], 1000, 1);
-sig=pammod(symbols, 2);
-sig=(sig+1)/2;
-figure(1)
-sig=complex(sig);
-t=[0:1:length(sig)-1];
-plot(t,sig,'o')
-title('segnale')
-xlabel('t')
+lung_sig = 2000
+T_symbol = 1/SamplingRate; % Tempo di simbolo
 
+symbols=zeros(1,Nsimboli);
+seq_start=[1,1,0,1,0,1,0,1];
+seq_end=[1,1,1,0,0,0,1,1];
 
+mes = [0,0,0,0,0,0,0,0,0,1,0,0,0,0,1,1,1];
 
-% sig= pammod(symbols, 2)*(1+i);
-% Design del filtro a coseno rialzato
+% seq_start = sprintf('%d',seq_start);
+% seq_end = sprintf('%d',seq_end);
+% mes = sprintf('%d',mes);
+
+message = horzcat(seq_start,mes,seq_end);
+% binArray=dec2bin(message,8)-'0';
+% binArray=binArray.';
+% binArray=reshape(binArray,1,33*8);
+% symbols=binArray;
+symbols=horzcat(message,zeros(1,967));
+symbols=symbols.';
+
+% symbols(500)=1;
+% symbols(497)=1;
+% symbols(750)=1;
+
 beta= 0.5; % Fattore di roll-off del filtro
 span = 6; % Lunghezza in simboli del filtro
 sps = 8;  % Campionamenti per simbolo (oversampling factor)
+
+
+%% Creazione segnale PAM
+sig=pammod(symbols,2);
+sig=(sig+1)/2; %rendiamo la modulazione unipodale
+sig_c=complex(sig);
+
+figure(1)
+
+stem(sig_c,'filled')
+title('Segnale')
+xlabel('Simboli')
+ylabel('Valore')
+axis("padded");
+
+%% Design del filtro a coseno rialzato
 
 txfilter=comm.RaisedCosineTransmitFilter(...
   Shape='Square root', ...
   RolloffFactor=beta, ...
   FilterSpanInSymbols=span, ...
   OutputSamplesPerSymbol=sps)
-% gain = 1 è di default
 
+% normalizzazione per avere a 1 il massimo del filtro 
+b = coeffs(txfilter);
+txfilter.Gain = 1/max(b.Numerator);
 
-% %normalizzazione per avere a 1 il massimo del filtro 
-% b = coeffs(txfilter);
-% txfilter.Gain = 1/max(b.Numerator);
-
-% disegno filtro
-figure(2);
+figure(2); % disegno filtro
 impz(txfilter.coeffs.Numerator);
-%il filtro è visto come una funzione e i punti servono per 
-% accedere ai vari campi di essa 
 
 %applico filtro
-tx_signal=txfilter([zeros(Nsimboli/2,1);sig;zeros(Nsimboli/2,1)]);
+tx_signal=txfilter([zeros(Nsimboli/2,1);sig_c;zeros(Nsimboli/2,1)]);
+
 %correzione delay tx
-tx_signal= tx_signal((span*sps)+1:end);
+tx_signal= tx_signal((span*sps/2)+1:end);
 
-t3=[0:1:length(tx_signal)-1];
 figure(3)
-plot(t3,tx_signal);
-axis("normal");
-
-
-% Fs = 100000;           % Frequenza di campionamento (Hz)
-% T_symbol = 1/Fs;      % Tempo di simbolo
-% rolloff_factor = 0.25; % Fattore di roll-off del filtro
-% 
-% % Design del filtro a coseno rialzato
-% span = 9; % Lunghezza in simboli del filtro
-% sps = 8;  % Campionamenti per simbolo (oversampling factor)
-% rcosine_filter = rcosdesign(rolloff_factor, span, sps,'sqrt');
-% figure(2)
-% plot(rcosine_filter,'.')
-% title('filtro tx')
-% 
-% % Generazione di un segnale di esempio
-% tx_signal = upfirdn(sig, rcosine_filter, sps);
 t3=[0:1:length(tx_signal)-1];
-figure(3)
 plot(t3,tx_signal);
-title('segnale tx filtrato');
-xlabel('t');
+axis("padded");
+title('Segnale Tx Filtrato');
+xlabel('Campioni');
+ylabel('Valori');
 
+%% Grafico Segnale e Segnale filtrato
+%variabile di supporto per grafico è segnale filtrato senza padding di
+%zeri
+tx_sig=txfilter(sig_c);
+tx_sig=tx_sig((span*sps/2)+1:end);
 
-% rcosine_filter = rcosdesign(rolloff_factor, span, sps,'sqrt');
-% rx_signal = upfirdn(tx_signal, rcosine_filter,1,8);
-% t4=[0:1:length(rx_signal)-1];
-% figure(4)
-% plot(t4,rx_signal)
-% title('segnale rx filtrato');
-% xlabel('t')
+figure(4)
+t= 1000*(0:length(sig_c)-1)*(sps/SamplingRate); 
+%vettore dei tempi campionato a passo di simbolo in millisecondi
+stem(t,sig_c,'rx');
+hold on
 
-txNorm=complex(tx_signal/max(abs(tx_signal)));
+to = 1000*(0:(length(tx_sig)-1))/SamplingRate;
+%vettore dei tempi campionato a frequenza di campionamento in millisecondi
+plot(to,tx_sig, 'b-'); 
+
+axis("padded");
+title('Segnale Generato e Filtrato');
+xlabel('Tempo in ms');
+ylabel('Valori');
+
+hold off;
+
+%% Trasmissione tramite Adalm Pluto 
+txNorm=tx_signal/max(abs(tx_signal));
+txNorm_c=complex(txNorm);
+
 txPluto = sdrtx('Pluto',...
        'RadioID',idTX,...
        'CenterFrequency',fc,...
        'Gain',-3, ... %must be between -89 and 0
        'BasebandSampleRate',SamplingRate);
 
-transmitRepeat(txPluto,txNorm);
+transmitRepeat(txPluto,txNorm_c);
 %100k simb per secondo
