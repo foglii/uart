9f997e423c394ca5d69b6e808074d337bd39c6b5
zixand <andrea.ziggiotti@mail.polimi.it>
2023-12-05 12:03:36 +0100

    graph update

--- grafico.m
+++ grafico.m
@@ -41,25 +41,38 @@ numberA(4) = 16;
 
 med = @(x) (1 - dataError(x,8)) .* (2 .* x + 9) + 2 .* (2*x+9) .* (dataError(x,8)) ./8;
 
+bitMediN = @(a,b) ((1-(2.^(1 - a) - 2.^(1 - b))).*(2 .* a + b + 1) + ((2.^(1-a)-2.^(1 - b))).* 2 .*(2.*a + b + 1))./ b;
+
 bitMedi16 = @(a) ((1-(2.^(1 - a)- 2.^(1 - 16))) .* (2 .* a + 17) + ((2.^(1 - a)- 2 .^(1-16))).*2.*(2 .* a + 17))./ 16;
 
 bitMedi8 = @(a) ((1-(2.^(1 - a) - 2.^(1- 8))).*(2 .* a +9) + ((2.^(1-a)-2.^(1 - 8))).* 2 .*(2.*a + 9))./ 8;
 
 error1 = @(M,x) ((errorPAM(M)./log2(numberA(M))).*(2 .* x +9).*2.*bitMedi8(x)) + (1 - ((errorPAM(M)./log2(numberA(M))).*(2 .* x + 9)).*bitMedi8(x));
-error2 = @(M,x) ((errorPAM(M)./log2(numberA(M))).*(2 .* x +17).*2.*bitMedi16(x)) + (1 - ((errorPAM(M)./log2(numberA(M))).*(2 .* x + 17)).*bitMedi16(x));
+error2 = @(M,x) (1./((1- (errorPAM(2)./log2(numberA(2)))).^(2 .* x + M + 1))).*bitMediN(x,M);
 
 
-
-x = 1:1:4; 
-y = 1:1:16;
+x = 1:1:10000; 
+y = 1:1:10000;
 [X, Y] = meshgrid(x, y);
 
-figure(1);
- mesh(X,Y,error1(X,Y));
+figure(7)
+mesh(X,Y,bitMediN(X,Y));
+xlabel("numero bit start_seq/end_seq ");
+ylabel("numero bit data");
 
+x = 1:1:20; 
+y = 1:1:56;
+
+[X, Y] = meshgrid(x, y);
  figure(6);
  mesh(X,Y,error2(X,Y));
- % set(gca,'ZScale','log')
+ylabel("numero bit start_seq/end_seq ");
+xlabel("numero bit data");
+
+% 
+% figure(1);
+%  mesh(X,Y,error1(X,Y));
+%  % set(gca,'ZScale','log')
 
 figure(2);
 fplot(bitMedi8,[0 25]);
