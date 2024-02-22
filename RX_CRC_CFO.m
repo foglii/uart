b325fdf8a98c00b1485f3fbe4d44a9429af7ef77
foglii <chiara.puggelli@mail.polimi.it>
2024-02-22 18:54:09 +0100

    Update RX_CRC_CFO.m

--- RX_CRC_CFO.m
+++ RX_CRC_CFO.m
@@ -10,6 +10,8 @@
 
 close all;
 load preamble.mat
+
+%preamble=txNorm_c(1:sps*length(seq_start));
 %clear;
 
 % Specifiche Adalm Pluto
@@ -20,15 +22,9 @@ idTX=append('sn:', Mypluto.SerialNum);
 SamplingRate=1e6;
 T_symbol = 1/SamplingRate;   % Tempo di simbolo
 fc=2.475e9;
-lung_sig = 2000;
-
-cmax=0;
-offset=0;
-
-Nsearch = 20;
+lung_sig = 72;
 
 %sequenze conosciute
-%seq_start=[1,1,0,1,0,1,0,1];
 barker = comm.BarkerCode("Length",13,"SamplesPerFrame",16);
 seq_start=barker().';
 seq_end=[1,1,1,0,0,0,1,1,];
@@ -43,42 +39,43 @@ rxPluto = sdrrx('Pluto','RadioID',...
     'GainSource','AGC Fast Attack',...
      'Gain',40,...
      'OutputDataType','single',...
-     'BasebandSampleRate',SamplingRate,...
-     'ShowAdvancedProperties',1,...
-     'FrequencyCorrection',100);
+     'BasebandSampleRate',SamplingRate);
 
- tic;   
-[rxWave,rx_meta]=capture(rxPluto,80000);
+ tic;  
+
+rxWave=capture(rxPluto,80000);
+
 toc;
+
+%aggiungere legenda
 t5=0:1:length(rxWave)-1;
 figure,
 title('RxWave');
 plot(t5,real(rxWave),t5,imag(rxWave));
+
+
 rxWave=rxWave/mean(abs(rxWave));
 
 % I campioni raccolti sono ora disponibili in rxWave
 
 %% Grafici Segnale Ricevuto
-% this will plot the absolute value of the samples
-
 figure
-n1 = (0:length(rxWave)-1)/(lung_sig*sps);
+n1= 1000*(0:length(rxWave)-1)*(sps/SamplingRate);  %controllare se è giusto
+%n1 = (0:length(rxWave)-1)/(lung_sig*sps);
 plot(n1,rxWave);
 title('Segnale Ricevuto')
 grid on;
 title('Segnale Ricevuto');
-xlabel('Numero Trasmissioni');
+xlabel('tempo in ms');
 ylabel('Valori');
 axis("padded");
 
 pause
-
 scatterplot(rxWave);
 pause
-M=2;
 
 constdiagram = comm.ConstellationDiagram( ...
-    'ReferenceConstellation',pammod(0:M-1,M), ...
+    'ReferenceConstellation',pammod(0:1,2), ...
     'ChannelNames',{'Before convergence','After convergence'}, ...
     'ShowLegend',true, ...
     'SamplesPerSymbol',sps, ...
@@ -86,6 +83,7 @@ constdiagram = comm.ConstellationDiagram( ...
     'SymbolsToDisplay',10000, ...
     'XLimits',[-1.5 1.5], ...
     'YLimits',[-1.5 1.5]);
+
 coarseSync = comm.CoarseFrequencyCompensator( ...
     'Modulation','PAM', ...
     'FrequencyResolution',1, ...
@@ -97,38 +95,32 @@ fineSync = comm.CarrierSynchronizer( ...
     'NormalizedLoopBandwidth',0.0005, ...
     'SamplesPerSymbol',sps, ...
     'Modulation','PAM');
+
 [syncCoarse,ritardo] = coarseSync(rxWave);
+
 rxSyncSig = fineSync(syncCoarse);
 rxSyncSig=rxSyncSig/mean(abs(rxSyncSig));
+
 constdiagram([rxSyncSig(1:10000) rxSyncSig(70001:80000)])
-rxSyncSig=rxSyncSig(70001:end);
-%preamble=txNorm_c(1:sps*length(seq_start));
-%[cplus,lag_start] = xcorr(pamdemod(rxSyncSig,2),pamdemod(barker_code);
-[cplus,lag_start] = xcorr(rxSyncSig,preamble);
+%prendo l'ultima parte del segnale, perchè più sincronizzata
+rxSyncSig=rxSyncSig(60001:end); 
+
+%ci allineiamo pre filtraggio tramite correlazione con un preambolo 
+[cros,lag_start] = xcorr(rxSyncSig,preamble);
+
 figure
-plot(abs(cplus))
-[peak,idx_shift]=max(abs(cplus));  % Trovo il picco della xcorr
-if idx_shift>15000
-    tmp=cplus;
-    tmp(15000:end)=0;
+plot(abs(cros)) %abbellire
+
+[peak,idx_shift]=max(abs(cros));  % Trovo il picco della xcorr
+
+if idx_shift>30000 || idx_shift<20000 %scelta di un picco nella prima metà della trasmissione
+    tmp=cros;
+    tmp(30000:end)=0;
+    tmp(1:20000)=0;
     [peak,idx_shift]=max(abs(tmp));
 end 
-sample_shift=idx_shift-length(rxSyncSig)+1; %Questo è lo shift da applicare
-
-
-
-% Qui controllate se ricevete dritto o girato di 180°
-% L'unico modo per "raddrizzarlo" automaticamente
-% sarebbe usare una sequenza di training
-% Fate un check se vi viene bit error rate vicino a 1 o vicino a zero
-% oppure controllate lo start bit che dovrebbe essere 1, se vi viene zero
-% girate tutto (questo però è rischioso con BER alti)
-a=rxSyncSig(sample_shift:end); %prendo giusto il pezzo da dove inizia
-figure,
-plot(real(a)) % -a perchè mi viene girata di 180°
-hold on, grid on,
-plot(real(preamble))
-
+sample_shift=idx_shift-length(rxSyncSig)+1;
+%Questo è lo shift da applicare
 
 
 % Design del filtro a coseno rialzato
@@ -138,13 +130,17 @@ rxfilter = comm.RaisedCosineReceiveFilter( ...
     'FilterSpanInSymbols',span, ...
     'InputSamplesPerSymbol',sps, ...
     'DecimationFactor',sps,...
-    'Gain',10,...
-    DecimationOffset=offset);
-%^ Per evitare di usare l'offset qui ci si può allineare prima
-% con la cross correlazione
+    'Gain',10);
 
 
-% Il meno è per via del fatto che viene girato di 180°
+%controllo se ricevo girato di 180° con il preambolo e poi correggo 
+%plot della parte iniziale
+a=rxSyncSig(sample_shift:sample_shift+length(preamble)); 
+figure, %abbellisci
+plot(real(a)) % -a perchè mi viene girata di 180°    
+hold on, grid on,
+plot(real(preamble))
+
 rxFiltSig=rxfilter(rxSyncSig(sample_shift:end)); 
 [~,b]=biterr(pamdemod(rxFiltSig((span+1):(span+16)),2),pamdemod(seq_start,2).');
 if b>0.5
@@ -155,13 +151,13 @@ end
 rxFiltSig=rxFiltSig/mean(abs(rxFiltSig));
 
 % Check sull'allineamento del segnale (con buon SNR si vede chiaramente)
-figure
-plot(real(rxFiltSig((span+1):end)))
-hold on, grid on,
-plot(real(sig_c));
+% figure
+% plot(real(rxFiltSig((span+1):end)))
+% hold on, grid on,
+% plot(real(sig_c));
 
 
- 
+ %plot post filtraggio 
  constDiagram = comm.ConstellationDiagram( ...
      'ReferenceConstellation',pammod(0:M-1,M), ...
     'ChannelNames',{'Sequenza Filtrata'}, ...
@@ -171,27 +167,32 @@ plot(real(sig_c));
 
 
 constDiagram(rxFiltSig)
-sigdemod=pamdemod(rxFiltSig(span+1:end),2).';
-clear readData; 
-readData = struct; 
 
+sigdemod=pamdemod(rxFiltSig(span+1:end),2).';
+
+%% Spacchettamento
+clear readData; 
+
+readData = struct; 
 readData.packNumber = [];      
 readData.data = [];
 readData.crcOK = [];
-readData.i=[];
+readData.i=[]; %abbellisci
 readData.flag=[];
-frame=sigdemod(1:72);
+
+frame=sigdemod(1:72); 
 sigdemod_f=sigdemod;
 sigdemod_f(1:72)=zeros(1,72);
-i=1;
+
+delay=1;
 for index=1:floor((length(sigdemod)/72)-1)
 
-    readData(index).i=i;
+    readData(index).i=delay;
     [readData(index).packNumber, readData(index).data, readData(index).crcOK] = unpackMessage(frame);
     readData(index).flag=0;
-    [i,sigdemod_f,frame]=findDelay(seq_start,sigdemod_f);
+    [delay,sigdemod_f,frame]=findDelay(seq_start,sigdemod_f);
     if frame==0
-         [i,sigdemod_f,frame]=findDelay(seq_start,sigdemod_f);
+         [delay,sigdemod_f,frame]=findDelay(seq_start,sigdemod_f);
     end
    
 
@@ -226,18 +227,27 @@ while index<=Npack
                 word = [ word readData(in).data]; %concatena tutte le parole in base al loro indice 
                 readData(in).flag=1;
                 index= index + 1;
-                in=1;
+                in=0;
                   
         end
         in =in +1;
-
+  
+    end
+    if in>floor((length(sigdemod)/72)-1) && index<=Npack
+        disp('pacchetto non ricevuto correttamente')
+        sbagliato=true;
+        break;
+    else 
+        sbagliato=false;
     end
       
 end
-
+if sbagliato==false
 fprintf('Hai ricevuto:\n %s', word)
+end
+%% Prestazioni
 %%EVM
-
+evm=[];
 counter=0;
 for n=1:length(readData)
     if readData(n).flag==1
@@ -249,91 +259,18 @@ for n=1:length(readData)
     end
 end
 evm=min(dist1,dist_1);
+figure;
+bar(evm.','DisplayName','evm')
 evm_medio=mean(reshape(evm.',1,Npack*72))
+%%MER
+mer=comm.MER(ReferenceSignalSource="Estimated from reference constellation", ...
+    ReferenceConstellation=pammod(0:1,2));
+MER=mer(rxFiltSig(span+1:end));
+fprintf('MER=%f dB',MER)
 
-
-
-
-
-% cros(h:h+63)=0;
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% 
-% figure (20)
-% plot(i_cros,cros)
-% 
-% pause
-% 
-% %clear readData; 
-% readData = struct; 
-% 
-% readData.packNumber = [];      
-% readData.data = [];
-% readData.crcOK = [];
-% 
-% 
-% [i,h,frame]=findDelay(cros,i_cros,sigdemod);
-% [readData(1).packNumber, readData(1).data, readData(1).crcOK] = unpackMessage(frame);
-% 
-% cros(h:h+63)=0;
-% 
-% 
-% for index=2:Nsearch
-%       %azzera cross per pacchetti successivi 
-%      [i,h,frame]=findDelay(cros,i_cros,sigdemod);
-% 
-%      [readData(index).packNumber, readData(index).data, readData(index).crcOK] = unpackMessage(frame);
-%      cros(h:h+63)=0;
-% end
-% 
-%            
-% %trova numero pacchetti totale 
-% for index=(1:Nsearch)
-%     
-%     if (readData(index).crcOK == 1)
-%         Npack = readData(index).packNumber; 
-%         
-%         if (readData(index).packNumber>Npack)
-%              Npack = readData(index).packetNumber;
-%         end      
-%     end
-% end
-% 
-% in=1;
-% index = 1;
-% word = '';
-% 
-% while (index<=Npack)
-%     
-%     while (in<=Nsearch)
-% 
-%         if (readData(in).crcOK == 1 & readData(in).packNumber == index)
-%                 word = [ word readData(in).data]; %concatena tutte le parole in base al loro indice 
-%                 
-%                 index= index + 1;
-%                 in=1;
-%                   
-%         end
-%         in =in +1;
-% 
-%     end
-%       
-% end
-% 
-% fprintf('Hai ricevuto:\n %s', word)
-% 
-% 
 %% Funzione che spacchetta messaggio
-% installa data aquisition toolbox
-
-% Data una stringa contenente solamente il
-% pacchetto su cui bisogna fare l'unpack, 
-% ossia seq_start + message + parity +seq_end 
+%Dato un frame di bit ne estrae il numero del pacchetto, i dati e controlla
+%la validità del contenuto
 
 %[readData(1).packNumber, readData(1).data, readData(1).crcOK] = unpackMessage(frame);
 function [packetNum, data, crcCheck] = unpackMessage(rawData)
@@ -394,25 +331,13 @@ function [i,sigdemod,frame]=findDelay(seq_start,sigdemod)
         [c,c_lag]=xcorr(sigdemod,seq_start);
         [m,h] = max(c);
         i = c_lag(h);
+
+        %controllo se il pacchetto è parziale
         if i<length(sigdemod)-72
-        
-%         figure
-%         plot(c_lag,c,[i i],[-0.5 1],'r:')
-%         text(i+100,0.5,['Lag: ' int2str(i)])
-%         ylabel('c')
-%         axis tight
-%         title('Cross-Correlation')
-        %s1=sigdemod(i+1:end);
-       
-        frame = sigdemod(i+1:i+72);
+           frame = sigdemod(i+1:i+72);
         else 
            frame=zeros(1,72);
         end
          sigdemod(i+1:i+16)=zeros(1,16);
-        % figure;
-        % % plot(frame,'x')
-        % hold on
-        % % plot(seq_start,'go')
-        % xlim([0,20]);
-        % hold off
+       
 end
